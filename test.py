cdd11d25c7603c322d7e24b74f557640ddea772d
Davi Álefe <davialefe96@gmail.com>
2022-06-19 13:51:05 -0300

    Setting .gitignore

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 from RulkovNetwork import RulkovNetwork
 import jax.numpy as jnp
 
-adjacency_matrix=jnp.array([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]])
+# adjacency_matrix=jnp.array([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]])
+adjacency_matrix=jnp.array([[0]])
 network = RulkovNetwork(adjacency_matrix)
-while network.t < 20:
+network.nodes_x = jnp.array([[-1]])
+network.nodes_y = jnp.array([[-2.75]])
+network.nodes = jnp.array([[-1, -2.75]])
+network.alpha = jnp.array([[4.1]])
+while network.t < 3000:
     network.evolve()
+    print(network.local_maximizers)
+    print(f'Iteration {network.t}')
+    print(f'\t Nodes: {network.nodes}')
