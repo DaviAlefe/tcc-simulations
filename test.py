52b8eeb4c62b45ec0cbec67ea9e87440bbefc764
Davi Álefe <davialefe96@gmail.com>
2022-07-23 20:56:26 -0300

    Installing networkx package.

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
-from RulkovNetwork import RulkovNetwork
+from rulkov_network import RulkovNetwork
 import jax.numpy as jnp
 
-# adjacency_matrix=jnp.array([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]])
-adjacency_matrix=jnp.array([[0]])
-network = RulkovNetwork(adjacency_matrix)
-network.nodes_x = jnp.array([[-1]])
-network.nodes_y = jnp.array([[-2.75]])
-network.nodes = jnp.array([[-1, -2.75]])
-network.alpha = jnp.array([[4.1]])
-while network.t < 3000:
+
+adjacency_matrix=jnp.array([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]])
+# adjacency_matrix=jnp.array([[0]])
+network = RulkovNetwork(adjacency_matrix, w_max=0.1, w_0=0.1)
+# network.nodes_x = jnp.array([[1]])
+# network.nodes_y = jnp.array([[-1]])
+# network.nodes = jnp.concatenate((network.nodes_x, network.nodes_y), axis=1)
+# network.alpha = jnp.array([[4.1]])
+
+while network.t < 10:
     network.evolve()
-    print(network.local_maximizers)
     print(f'Iteration {network.t}')
     print(f'\t Nodes: {network.nodes}')
+    print(f'\t increment_count: {network.increment_count}')
