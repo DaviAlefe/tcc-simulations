775f0051cb0f0621059ccf1526e69685e73b9472
Davi Álefe <davialefe96@gmail.com>
2022-07-27 20:28:36 -0300

    Turning sw_simulation into a class object.

--- sw_simulation.py
+++ sw_simulation.py
@@ -4,40 +4,45 @@ import networkx as nx
 from rulkov_network import RulkovNetwork
 from datetime import datetime
 
-# Network Parameters
-N = 1000
-k = 4
-p = 0.2
 
-graph = watts_strogatz_graph(N, k, p)
-adjcency_matrix = nx.to_numpy_matrix(graph)
+class SWSimulation:
+    def __init__(self, w_0_mult):
+        # Network Parameters
+        self.N = 1000
+        self.k = 4
+        self.p = 0.2
 
-w_max = 0.2
-w_0 = 0.2*w_max
+        self.graph = watts_strogatz_graph(self.N, self.k, self.p)
+        self.adjcency_matrix = nx.to_numpy_matrix(self.graph)
 
-network = RulkovNetwork(adjcency_matrix, w_max, w_0)
+        self.w_max = 0.2
+        self.w_0 = w_0_mult * self.w_max
 
-# Simulation Parameters
-transient = 1e4
-T = 1.5e6
-# The network does not update weights for transient time steps
-network.freeze_weights = True
+        self.network = RulkovNetwork(self.adjcency_matrix, self.w_max, self.w_0)
 
-simulation_start = datetime.now()
-print(30*'-')
-print(f'Simulation started.')
-while network.t < T:
-    # Set the weights to be updated after transient time steps
-    if network.t == transient:
-        network.freeze_weights = False
-        print('End of transient period.')
+        # Simulation Parameters
+        self.transient = 1e4
+        self.T = 1.5e6
+    
+    def run(self):
+        # The network does not update weights for transient time steps
+        self.network.freeze_weights = True
 
-    network.evolve()
+        simulation_start = datetime.now()
+        print(30*'-')
+        print(f'Simulation started.')
+        while self.network.t < self.T:
+            # Set the weights to be updated after transient time steps
+            if self.network.t == self.transient:
+                self.network.freeze_weights = False
+                print('End of transient period.')
 
-    # Mark time every 500 iterations and predict the time remaining
-    if network.t % 500 == 0:
-        current_time = datetime.now()
-        time_elapsed = current_time - simulation_start
-        print(f'\t\t {network.t} iterations, {time_elapsed} elapsed.')
-        time_remaining = time_elapsed * (T - network.t) / network.t
-        print(f'\t\t Approximately {time_remaining} remaining.')
+            self.network.evolve()
+
+            # Mark time every 500 iterations and predict the time remaining
+            if self.network.t % 500 == 0:
+                current_time = datetime.now()
+                time_elapsed = current_time - simulation_start
+                print(f'\t\t {self.network.t} iterations, {time_elapsed} elapsed.')
+                time_remaining = time_elapsed * (self.T - self.network.t) / self.network.t
+                print(f'\t\t Approximately {time_remaining} remaining.')
