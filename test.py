64f3af4a03f96521294116afcf16af022bad784d
Davi Álefe <davialefe96@gmail.com>
2022-07-29 18:41:18 -0300

    Bug fix: fixing linear algebra in the update_weights method

--- test.py
+++ test.py
@@ -11,7 +11,8 @@ network = RulkovNetwork(adjacency_matrix, w_max=0.1, w_0=0.1)
 # network.alpha = jnp.array([[4.1]])
 
 while network.t < 10:
-    network.evolve()
+    network.update_weights([1])
+    network.t +=1
     print(f'Iteration {network.t}')
     print(f'\t Nodes: {network.nodes}')
     print(f'\t increment_count: {network.increment_count}')
