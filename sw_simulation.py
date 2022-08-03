c6ecffc56557ede7e037da398054da39ade823cf
Davi Álefe <davialefe96@gmail.com>
2022-08-03 13:47:44 -0300

    Adding logging

--- sw_simulation.py
+++ sw_simulation.py
@@ -3,12 +3,15 @@ from networkx import watts_strogatz_graph
 import networkx as nx
 from rulkov_network import RulkovNetwork
 from datetime import datetime
+import logging
 
 
 class SWSimulation:
     def __init__(self, w_0_mult):
+        self.simulation_id=f'simulation_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+
         # Network Parameters
-        self.N = 1000
+        self.N = 100
         self.k = 4
         self.p = 0.2
 
@@ -18,24 +21,32 @@ class SWSimulation:
         self.w_max = 0.2
         self.w_0 = w_0_mult * self.w_max
 
-        self.network = RulkovNetwork(self.adjcency_matrix, self.w_max, self.w_0)
+        self.network = RulkovNetwork(self.adjcency_matrix, self.w_max, self.w_0,
+         simulation_id=self.simulation_id)
 
         # Simulation Parameters
         self.transient = 1e4
         self.T = 1.5e6
+
+        # Logging
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s',
+        filename=f'./simulations_data/{self.simulation_id}.log', filemode='w', force=True)
+        # Log the simulation parameters
+        logging.info(f'Simulation parameters:')
+        logging.info(str(self.__dict__))
     
     def run(self):
         # The network does not update weights for transient time steps
         self.network.freeze_weights = True
 
         simulation_start = datetime.now()
-        print(30*'-')
-        print(f'Simulation started.')
+        logging.info(30*'-')
+        logging.info(f'Simulation started.')
         while self.network.t < self.T:
             # Set the weights to be updated after transient time steps
             if self.network.t == self.transient:
                 self.network.freeze_weights = False
-                print('End of transient period.')
+                logging.info('End of transient period.')
 
             self.network.evolve()
 
@@ -43,6 +54,6 @@ class SWSimulation:
             if self.network.t % 100 == 0:
                 current_time = datetime.now()
                 time_elapsed = current_time - simulation_start
-                print(f'\t\t {self.network.t} iterations, {time_elapsed} elapsed.')
+                logging.info(f'\t\t {self.network.t} iterations, {time_elapsed} elapsed.')
                 time_remaining = time_elapsed * (self.T - self.network.t) / self.network.t
-                print(f'\t\t Approximately {time_remaining} remaining.')
+                logging.info(f'\t\t Approximately {time_remaining} remaining.')
