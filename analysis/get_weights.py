Fix: wrong formula for updating weights. Only the variations in weights should be masked by updated neurons.

analysis/get_weights.py
@@ -7,5 +7,35 @@ analysis_db_path = '/home/davialefe/tcc/simulations/analysis/data.db'
 def get_weights(simulation_id):
     # get the paths to the weights matrices saved as npy files inside the folder
     weights_matrices_files = [filename for filename in os.listdir(os.path.join(simulations_data_path, simulation_id))  if \
-        filename.endswith(f'_weights_simulation_{simulation_id}.npy')]
-    print(weights_matrices_files)
+        filename.endswith(f'_weights_{simulation_id}.npy')]
+    # load the weights matrices
+    initial_weights_file = [filename for filename in weights_matrices_files if filename.startswith('20000')][0]
+    initial_weights = np.load(os.path.join(simulations_data_path, simulation_id, initial_weights_file))
+    final_weights_file = [filename for filename in weights_matrices_files if filename.startswith('1500000')][0]
+    final_weights = np.load(os.path.join(simulations_data_path, simulation_id, final_weights_file))
+    # load the adj_matrix.npy file in the folder
+    adj_matrix = np.load(os.path.join(simulations_data_path, simulation_id, 'adj_matrix.npy'))
+
+    initial_weights = np.multiply(initial_weights, adj_matrix)
+    final_weights = np.multiply(final_weights, adj_matrix)
+
+    # calculate the mean of the weights matrices
+    initial_weights_mean = np.sum(initial_weights)/np.sum(adj_matrix)
+    final_weights_mean = np.sum(final_weights)/np.sum(adj_matrix)
+
+    # divide by the networks' max weight
+    initial_weights_mean /= 0.2
+    final_weights_mean /= 0.2
+
+    return (initial_weights_mean, final_weights_mean)
+
+if __name__ == '__main__':
+    # open the analysis database and get the associations table as a df
+    conn = sqlite3.connect(analysis_db_path)
+    associations = pandas.read_sql_query('SELECT * FROM associations', conn)
+    # for each simulation_id in the associations table, get the initial and final window kuramoto
+    associations['initial_mean_weight'], associations['final_mean_weight'] = zip(*associations['simulation_id'].apply(get_weights))
+    # save the associations table to the analysis database
+    associations.to_sql('associations', conn, if_exists='replace', index=False)
+    # close the connection
+    conn.close()

rulkov_network.py
@@ -178,7 +178,7 @@ class RulkovNetwork:
             delta_w = jnp.zeros((self.n, self.n))
 
             # low_dt_mask is a nxn ones matrix where delta_t is lesser than Ts and 0 otherwise.
-            low_dt_mask = jnp.where(self.delta_t < self.Ts, jnp.ones((self.n, self.n)), jnp.zeros((self.n, self.n)))
+            low_dt_mask = jnp.where(jnp.abs(self.delta_t) < self.Ts, jnp.ones((self.n, self.n)), jnp.zeros((self.n, self.n)))
             rel_ampl = ((self.Ap - self.Ad)/self.Ts)
             # low_dt_dw is the matrix with weights variations wherever delta_t < Ts.
             low_dt_dw = self.Ap*low_dt_mask - rel_ampl*jnp.multiply(jnp.abs(self.delta_t), low_dt_mask)
@@ -186,7 +186,7 @@ class RulkovNetwork:
             delta_w = delta_w + low_dt_dw
 
             # high_dt_mask is a nxn ones matrix where delta_t is greater than Ts and 0 otherwise.
-            high_dt_mask = jnp.where(self.delta_t > self.Ts, jnp.ones((self.n, self.n)), jnp.zeros((self.n, self.n)))
+            high_dt_mask = jnp.where(jnp.abs(self.delta_t) > self.Ts, jnp.ones((self.n, self.n)), jnp.zeros((self.n, self.n)))
             # high_dt_dw is the matrix with weights variations wherever delta_t > Ts.
             high_dt_dw = self.Ad * high_dt_mask
             delta_w = delta_w + high_dt_dw
@@ -199,7 +199,7 @@ class RulkovNetwork:
             # Is repeated to a nxn matrix.
             neurons_mask = neurons_mask.T.repeat(self.n, axis=0).T
             # The weights matrix is updated with the delta_w matrix, at the neurons to update.
-            self.weights = jnp.multiply((self.weights + delta_w), neurons_mask)
+            self.weights = self.weights + jnp.multiply(delta_w, neurons_mask)
 
             # The weights matrix is brought to w_max where the weights are greater than w_max.
             self.weights = self.weights.at[jnp.where(self.weights > self.w_max)].set(self.w_max)

sw_simulation.py
@@ -8,8 +8,8 @@ import logging, sys, json, os
 
 
 class SWSimulation:
-    def __init__(self, w_0_mult, base_dir='.'):
-        self.simulation_id=f'simulation_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+    def __init__(self, w_0_mult, simulation_id = None, base_dir='.'):
+        self.simulation_id= simulation_id if simulation_id else f'simulation_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
         self.base_dir = base_dir
 
         # Network Parameters
@@ -32,7 +32,7 @@ class SWSimulation:
 
         # Simulation Parameters
         self.transient = 1e4
-        self.T = 1.5e6
+        self.T = 1.2e4#1.5e6
 
         # Logging
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s',
@@ -103,6 +103,10 @@ class SWSimulation:
                 logging.info(f'\t\t {self.network.t} iterations, {time_elapsed} elapsed.')
                 time_remaining = time_elapsed * (self.T - self.network.t) / self.network.t
                 logging.info(f'\t\t Approximately {time_remaining} remaining.')
+
+                print(f'\t\t Freeze weights state: {self.network.freeze_weights}')
+                print(f'\t\t Mean weight: {np.sum(self.network.weights)/(0.2*np.sum(self.network.adjacency_matrix))}')
+                print(f'\t\t Mean delta t: {np.sum(np.abs(self.network.delta_t))/np.count_nonzero(self.network.delta_t)}')
             
             
             # Save the network weights at t=2e4

test.py
@@ -1,15 +1,16 @@
 import os, sqlite3
+import numpy as np
+from rulkov_network import RulkovNetwork
 
-simulations_data_path = '/home/davialefe/tcc/simulations/simulations_data'
-# for each folder in simulations_data, open the .db file and delete the InitialKuramoto and FinalKuramoto tables
-for _ in os.listdir(simulations_data_path):
-    if os.path.isdir(os.path.join(simulations_data_path, _)):
-        db_name = [filename for filename in os.listdir(os.path.join(simulations_data_path, _))  if \
-            filename.endswith('.db')][0]
-        if db_name:
-            db_path = os.path.join(simulations_data_path, _, db_name)
-            conn = sqlite3.connect(db_path)
-            conn.execute('DROP TABLE IF EXISTS InitialKuramoto')
-            conn.execute('DROP TABLE IF EXISTS FinalKuramoto')
-            conn.commit()
-            conn.close()
+# create a 4x4 adjacency matrix
+adj_matrix = np.array([[0, 1, 1, 1],
+                          [1, 0, 1, 1],
+                            [1, 1, 0, 1],
+                                [1, 1, 1, 0]])
+
+# create the delta_t 4x4 matrix as a numpy array with random positive floats
+delta_t = np.random.rand(4, 4)
+w_max = 0.2
+# create a rulkov network object
+network = RulkovNetwork(adj_matrix, w_max, 0.1, simulation_id='test', save_weights_mode=False, save_nodes_mode=False, save_maxima_mode=False)
+network.delta_t = delta_t